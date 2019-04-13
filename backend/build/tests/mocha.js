f6c7a64f04389ac46d3e27a49f50a9f5a18430e0
mario <likearollingstone199@gmail.com>
2019-04-13 23:48:02 +0100

    Created the sceleton backend with typescript

---
+++ backend/build/tests/mocha.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+if (process.env.ENV !== 'test') {
+    console.warn('It is forbidden to run tests in any other environment other than test. Running environment is ' + process.env.ENV);
+    process.exit(1);
+}
+const Mocha = require('mocha');
+const fs = require('fs');
+const path = require('path');
+const mocha = new Mocha({
+    ui: 'bdd',
+    reporter: 'list'
+});
+const testDir = path.resolve(__dirname);
+const directories = [];
+directories.push(path.resolve(__dirname));
+directories.push(path.resolve(__dirname, 'unit'));
+for (let dir of directories) {
+    fs.readdirSync(dir).filter(function (file) {
+        return file.substr(-3) === '.js';
+    }).forEach(function (file) {
+        mocha.addFile(path.join(dir, file));
+    });
+}
+mocha.run(function (failures) {
+    process.on('exit', function () {
+        process.exit(failures);
+    });
+});
