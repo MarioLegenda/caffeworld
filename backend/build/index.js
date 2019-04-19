d94a5a8364f84470ab8582efb41e9b30a9d33120
mario <likearollingstone199@gmail.com>
2019-04-19 18:58:30 +0100

    Added sockets on the list

--- backend/build/index.js
+++ backend/build/index.js
@@ -4,13 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
+require('dotenv').config();
+const http = require('http');
 const express = require('express');
 const app = express();
 const port = 3000;
 const ContainerWrapper_1 = __importDefault(require("./src/container/ContainerWrapper"));
 const redis_1 = __importDefault(require("./src/dataSource/redis"));
-require('dotenv').config();
+const io = require("socket.io")(http, { pingTimeout: 60000 });
 redis_1.default.client.on('ready', () => {
     ContainerWrapper_1.default.createContainer().bindDependencies();
-    app.listen(port, () => console.log(`App listening on port ${port}!`));
+    app.listen(port, () => {
+        io.on('connection', function (socket) {
+            console.log('a user connected');
+            socket.on('disconnect', function () {
+                console.log('user disconnected');
+            });
+        });
+        console.log('Application started. Listening on port 3000');
+    });
 });
--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,29 @@
 import 'reflect-metadata';
+
+require('dotenv').config();
+
 import {Express} from "express";
+
+const http = require('http');
 const express = require('express');
 const app: Express = express();
 const port = 3000;
 import ContainerWrapper from "./src/container/ContainerWrapper";
 import Redis from "./src/dataSource/redis";
-
-require('dotenv').config();
+const io = require("socket.io")(http, { pingTimeout: 60000 });
 
 Redis.client.on('ready', () => {
     ContainerWrapper.createContainer().bindDependencies();
 
-    app.listen(port, () => console.log(`App listening on port ${port}!`))
+    app.listen(port, () => {
+        io.on('connection', function (socket: any) {
+            console.log('a user connected');
+
+            socket.on('disconnect', function () {
+                console.log('user disconnected');
+            });
+        });
+
+        console.log('Application started. Listening on port 3000');
+    });
 });
