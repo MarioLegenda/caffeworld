0aa504df5dcdb131ad216c1fcd3ebf5517c1ecc1
mario <likearollingstone199@gmail.com>
2019-04-21 16:25:44 +0100

    Removed backend build dir from being tracked by git

--- backend/build/src/container/Symbols.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Symbols = {
-    SocketCommunicator: Symbol.for('SocketCommunicator'),
-    TableEvent: Symbol.for('TableEvent'),
-    TableService: Symbol.for('TableService'),
-    ObservableFactory: Symbol.for('ObservableFactory'),
-};
