c5e167323d0d7432cea6689235cc6cb68775a744
mario <likearollingstone199@gmail.com>
2019-04-24 13:09:24 +0100

    Fixed the socke/rxjs issues for create/table route. APP NOT FUNCTIONING, DO NOT REVERT/RESET TO THIS COMMIT

---
+++ frontend/src/app/caffeeworld/infrastructure/observableFactory/OnceObservableFactory.ts
@@ -0,0 +1,34 @@
+import {Injectable} from "@angular/core";
+import {Subject} from "rxjs";
+import IObservableFactory from "./IObservableFactory";
+
+@Injectable()
+export default class ObservableFactory implements IObservableFactory{
+    private observable: Subject<any>;
+    private name: string;
+
+    createObservable(name: string): void {
+        this.name = name;
+        this.observable = new Subject();
+    }
+
+    hasObservable(name: string): boolean {
+        return name === this.name;
+    }
+
+    getObservable(name: string): Subject<any> {
+        if (name !== this.name) throw new Error(`ObservableFactory error. Current observable is ${this.name} but you wanted ${name}. Create a new observable with the name '${name}' and call ObservableFactory::getObservable() with that name`);
+
+        return this.observable;
+    }
+
+    unsubscribe(): void {
+        this.observable.unsubscribe();
+        this.observable = null;
+        this.name = null;
+    }
+
+    get subscriberCount() {
+        return this.observable.observers.length;
+    }
+}
