15eab2c7203cb48a6ed7a759b8396298c0d2d1da
mario <likearollingstone199@gmail.com>
2019-05-08 08:49:49 +0100

    imported webrtc adapter into main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import adapter from 'webrtc-adapter';
+
 if (environment.production) {
   enableProdMode();
 }
